38e4de34b1d99c3e4dc206f64afba612ce0e46b6
srishti-lamba <72405276+TaeDaeTao@users.noreply.github.com>
2026-01-07 19:42:36 -0500

    database structure

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react'
 import { render, screen } from '@testing-library/react';
 import App from './App';
+
+// Run once
+  useEffect(() => {
+  }, [])
 
 test('renders learn react link', () => {
   render(<App />);
